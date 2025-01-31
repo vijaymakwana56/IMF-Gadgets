7ee888b0229dd58a3fa40599ccc8e462267d9ce7
Vijay Makwana <vijaygm20@iitk.ac.in>
2025-01-31 21:45:33 +0530

    feat: db url added

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,19 @@ import {Sequelize} from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME,process.env.PASSWORD, {
+// const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME,process.env.PASSWORD, {
+//     dialect: "postgres",
+//     port: 5432,
+//     dialectOptions: {
+//         ssl: {
+//           require: true,
+//           rejectUnauthorized: false,
+//         },
+//       },
+//     logging: false,
+// });
+const sequelize = new Sequelize(process.env.DB_URL, {
     dialect: "postgres",
-    port: 5432,
     dialectOptions: {
         ssl: {
           require: true,
